a152ef7b735f2edb264d96fce06f88562dd7d676
Erli <85597743+Erli-Erli@users.noreply.github.com>
2021-12-07 12:51:06 +0700

    Project

---
+++ app/src/main/java/com/example/hallotoast/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hallotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this,R.string.messsage_Toast, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+    public void Countup(View view){
+
+    }
+}
